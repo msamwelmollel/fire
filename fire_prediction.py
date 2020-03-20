16f4ea5a4f50e03f90671ac9194dc75090f47792
msamwelmollel <47386502+msamwelmollel@users.noreply.github.com>
2020-03-20 18:57:57 +0300

    Create fire_prediction.py

---
+++ fire_prediction.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 19 22:54:43 2020
+
+@author: Team Mandela 
+"""
+# Use some functions from tensorflow_docs
+
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import pathlib
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+#print(tf.__version__)
+
+path = "C:/Users/msamw/Google Drive/fire/forestfires.csv"
+
+dataset = pd.read_csv(path,sep=',')
+
+dataset.head()
+
+dataset.isna().sum()
+
+
+train_dataset = dataset.sample(frac=0.8,random_state=0)
+test_dataset = dataset.drop(train_dataset.index)
+
+
+sns.pairplot(train_dataset[["X", "Y", "FFMC", "DMC","ISI","temp","RH","wind","rain","area"]], diag_kind="kde")
+
+
