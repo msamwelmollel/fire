a2c777e42843667990f7ea57c3db8a244d5535bc
msamwelmollel <47386502+msamwelmollel@users.noreply.github.com>
2020-03-21 13:14:43 +0300

    Update zindi_fire.py

--- zindi_fire.py
+++ zindi_fire.py
@@ -22,7 +22,28 @@ from sklearn import preprocessing
 
 path = "train.csv"
 
-train_dataset = pd.read_csv(path,sep=',')
+train_dataset = pd.read_csv(path, parse_dates=['date'])
+test_dataset = pd.read_csv('test.csv',sep=',', parse_dates=['date'])
 
-train_dataset.head()
+#print(train_dataset.head())
+#%%
+# looking for features to remove 
+train_dataset.corr()['burn_area'].sort_values().plot(kind='bar', figsize=(18,6))
+
+value = train_dataset.corr()['burn_area'].sort_values()
+#print(value.iloc[:])
+
+value = value[value<0.2]
+value = value[value>-0.2]
+#print(value)
+value = list(value.index)
+#print(value)
+
+train_dataset = train_dataset.drop(value, axis=1)
+#print(train_dataset.head())
+train_dataset.corr()['burn_area'].sort_values().plot(kind='bar', figsize=(18,6))
+
+
+
+
 
